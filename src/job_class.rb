cc2e58fdc1abec184ba9ab4ac9fdb8a24f149200
LachlynR <lachyreynolds@outlook.com>
2020-04-26 19:32:03 +1000

    Created Job Class

---
+++ src/job_class.rb
@@ -0,0 +1,15 @@
+require_relative 'functions'
+
+class Job
+
+    attr_accessor :position, :employer, :industry, :progress, :application_date, :notes
+
+    def initialize(position, employer, industry, progress, application_date, notes)
+        @position = position
+        @employer = employer
+        @industry = industry
+        @progress = progress
+        @application_date = application_date
+        @notes = notes
+    end
+end
